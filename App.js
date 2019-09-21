987f9fbf14d667b8605e6e43f54b8889b8d1076f
naniprabhakar <55038229+naniprabhakar@users.noreply.github.com>
2019-09-21 16:11:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+/*import './form1.js'; */
+
+function App() {
+  return (
+    <center>
+      <h1>Hi My Name Is Prabhakar</h1>
+      <div class="Box"> Apple</div>
+      <div class="Box"> Mango</div>
+      <div class="Box"> Banana</div>
+      <br></br>
+      <br></br>
+      <mark>HI Prabhakar</mark>
+      <br></br>
+      <br></br>
+      <marquee> Hi </marquee>
+      <br></br>
+      <br></br>
+      <i>hi How Are You?</i>
+      <br></br>
+      <br></br>
+      <table>
+        <tr><th>Name</th><th>ID</th><th>Phone</th></tr>
+        <tr><td>Prabhakar</td><td>25</td><td>9848012112</td></tr>
+        <tr><td>Pra</td><td>20</td><td>984801212</td></tr>
+        <tr><td>Prabha</td><td>45</td><td>9848012512</td></tr>
+      </table>
+      <br></br>
+      <br></br>
+      <ul>
+        <ol>One</ol>
+        <ol>Two</ol>
+        <ol>Three</ol>
+      </ul>
+
+    </center>
+  );
+}
+
+export default App;
